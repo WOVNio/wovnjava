3aac35d9689e90fce8e754479612d56a48f7cad0
Torkel Berli <torkelbe@gmail.com>
2019-06-07 11:26:30 +0900

    Strip language code and X-Wovn-Handler also for wovnDisable mode (#97)

--- src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
+++ src/main/java/com/github/wovnio/wovnjava/WovnServletFilter.java
@@ -30,24 +30,25 @@ public class WovnServletFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException
     {
+        ((HttpServletResponse)response).setHeader("X-Wovn-Handler", "wovnjava_" + Settings.VERSION);
+
         RequestOptions requestOptions = new RequestOptions(this.settings, request);
+        Headers headers = new Headers((HttpServletRequest)request, this.settings);
 
-        if (requestOptions.getDisableMode()) {
-            chain.doFilter(request, response);
+        String lang = headers.getPathLang();
+        boolean isRequestWithDefaultLanguageCode = settings.urlPattern.equals("path") && lang.length() > 0 && lang.equals(settings.defaultLang);
+        boolean canProcessRequest = !requestOptions.getDisableMode() && headers.isValidPath() && htmlChecker.canTranslatePath(headers.pathName);
+
+        if (isRequestWithDefaultLanguageCode) {
+            /* Send 302 redirect to equivalent URL without default language code */
+            ((HttpServletResponse) response).sendRedirect(headers.redirectLocation(settings.defaultLang));
+        } else if (canProcessRequest) {
+            /* Process the request */
+            tryTranslate(headers, requestOptions, (HttpServletRequest)request, (HttpServletResponse)response, chain);
         } else {
-            ((HttpServletResponse)response).setHeader("X-Wovn-Handler", "wovnjava_" + Settings.VERSION);
-
-            Headers headers = new Headers((HttpServletRequest)request, this.settings);
-            String lang = headers.getPathLang();
-            boolean hasShorterPath = settings.urlPattern.equals("path") && lang.length() > 0 && lang.equals(settings.defaultLang);
-            if (hasShorterPath) {
-                ((HttpServletResponse) response).sendRedirect(headers.redirectLocation(settings.defaultLang));
-            } else if (headers.isValidPath() && htmlChecker.canTranslatePath(headers.pathName)) {
-                tryTranslate(headers, requestOptions, (HttpServletRequest)request, (HttpServletResponse)response, chain);
-            } else {
-                WovnHttpServletRequest wovnRequest = new WovnHttpServletRequest((HttpServletRequest)request, headers);
-                chain.doFilter(wovnRequest, response);
-            }
+            /* Strip language code and pass through the request and response untouched */
+            WovnHttpServletRequest wovnRequest = new WovnHttpServletRequest((HttpServletRequest)request, headers);
+            chain.doFilter(wovnRequest, response);
         }
     }
 
