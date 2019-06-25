5a65988ef2091cfb0317a265bb08d9f3c9bbb8b2
Torkel Berli <torkelbe@gmail.com>
2019-06-25 09:38:15 +0900

    Fix tests

--- src/test/java/com/github/wovnio/wovnjava/SubdomainUrlLanguagePatternHandlerTest.java
+++ src/test/java/com/github/wovnio/wovnjava/SubdomainUrlLanguagePatternHandlerTest.java
@@ -25,17 +25,17 @@ public class SubdomainUrlLanguagePatternHandlerTest extends TestCase {
 
     public void testRemoveLang__NonMatchingSubdomain__DoNotModify() {
         SubdomainUrlLanguagePatternHandler sut = new SubdomainUrlLanguagePatternHandler();
-		assertEquals("/", sut.getLang("/"));
-        assertEquals("site.com", sut.getLang("site.com"));
-		assertEquals("ja.site.com", sut.getLang("ja.site.com", "fr"));
-		assertEquals("ja.fr.site.com", sut.getLang("ja.fr.site.com", "fr"));
-		assertEquals("site.com/fr/index.html?wovn=fr", sut.getLang("site.com/fr/index.html?wovn=fr", "fr"));
+		assertEquals("/", sut.removeLang("/", "en"));
+        assertEquals("site.com", sut.removeLang("site.com", "en"));
+		assertEquals("ja.site.com", sut.removeLang("ja.site.com", "fr"));
+		assertEquals("ja.fr.site.com", sut.removeLang("ja.fr.site.com", "fr"));
+		assertEquals("site.com/fr/index.html?wovn=fr", sut.removeLang("site.com/fr/index.html?wovn=fr", "fr"));
     }
 
     public void testRemoveLang__MatchingSubdomain__RemoveLangCode() {
         SubdomainUrlLanguagePatternHandler sut = new SubdomainUrlLanguagePatternHandler();
-		assertEquals("site.com", sut.getLang("en.site.com", "en"));
-		assertEquals("site.com/", sut.getLang("es.site.com/", "es"));
-		assertEquals("site.com/fr/index.html?lang=fr&wovn=fr", sut.getLang("fr.site.com/fr/index.html?lang=fr&wovn=fr", "fr");
+		assertEquals("site.com", sut.removeLang("en.site.com", "en"));
+		assertEquals("site.com/", sut.removeLang("es.site.com/", "es"));
+		assertEquals("site.com/fr/index.html?lang=fr&wovn=fr", sut.removeLang("fr.site.com/fr/index.html?lang=fr&wovn=fr", "fr"));
 	}
 }
