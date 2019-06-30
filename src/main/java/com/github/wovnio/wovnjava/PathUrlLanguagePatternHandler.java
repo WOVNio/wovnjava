e0467b5aa1d00fc1a7f1ec40334db86ace370028
Torkel Berli <torkelbe@gmail.com>
2019-06-30 23:22:03 +0900

    WIP fix removeLang for path urlPattern

--- src/main/java/com/github/wovnio/wovnjava/PathUrlLanguagePatternHandler.java
+++ src/main/java/com/github/wovnio/wovnjava/PathUrlLanguagePatternHandler.java
@@ -1,16 +1,23 @@
 package com.github.wovnio.wovnjava;
 
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class PathUrlLanguagePatternHandler extends UrlLanguagePatternHandler {
     static final String PATH_GET_LANG_PATTERN_REGEX = "/([^/.?]+)";
 
     private String sitePrefixPath;
     private Pattern getLangPattern;
+    private Pattern removeLangPattern;
 
     PathUrlLanguagePatternHandler(String sitePrefixPath) {
+        // placeholder supportedLangs
+        ArrayList<String> supportedLangs = new ArrayList<String>();
+        supportedLangs.add("en");
+
         this.sitePrefixPath = sitePrefixPath;
         this.getLangPattern = Pattern.compile(sitePrefixPath + PATH_GET_LANG_PATTERN_REGEX);
+        this.removeLangPattern = this.buildRemoveLangPattern(supportedLangs, sitePrefixPath);
     }
 
     String getLang(String url) {
@@ -18,11 +25,22 @@ class PathUrlLanguagePatternHandler extends UrlLanguagePatternHandler {
     }
 
     String removeLang(String url, String lang) {
-        String prefix = this.sitePrefixPath + "/";
-        return url.replaceFirst(prefix + lang + "(/|$)", prefix);
+        Matcher matcher = this.removeLangPattern.matcher(url);
+        return matcher.replaceFirst("$1$2$3");
     }
 
     String insertLang(String url, String lang) {
         return "site.com/en/path";
+    }
+
+    private Pattern buildRemoveLangPattern(ArrayList<String> supportedLangs, String sitePrefixPath) {
+        Pattern p = Pattern.compile(
+                "^(.*://)?" + /* optional schema */
+                "([^/]*)?" + /* optional host */
+                "(" + sitePrefixPath + "/)" + /* sitePrefixPath */
+                "(" + String.join("|", supportedLangs) + ")" + /* lang code */
+                "(/|$)"
+        );
+        return p;
     }
 }
