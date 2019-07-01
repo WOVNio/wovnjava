a4b18d4bf0f85c4eb18de048c243b252800e097b
Torkel Berli <torkelbe@gmail.com>
2019-07-01 14:34:48 +0900

    Retab url pattern handler tests

--- src/test/java/com/github/wovnio/wovnjava/QueryUrlLanguagePatternHandlerTest.java
+++ src/test/java/com/github/wovnio/wovnjava/QueryUrlLanguagePatternHandlerTest.java
@@ -17,26 +17,26 @@ public class QueryUrlLanguagePatternHandlerTest extends TestCase {
 
     public void testGetLang__MatchingQuery__ReturnLangCode() {
         QueryUrlLanguagePatternHandler sut = new QueryUrlLanguagePatternHandler();
-		assertEquals("fr", sut.getLang("?wovn=fr"));
-		assertEquals("fr", sut.getLang("/?wovn=fr"));
-		assertEquals("fr", sut.getLang("/en/?wovn=fr"));
-		assertEquals("fr", sut.getLang("/en/?lang=es&wovn=fr&country=vi"));
-		assertEquals("fr", sut.getLang("en.site.com/es/page/index.html?wovn=fr"));
-	}
+        assertEquals("fr", sut.getLang("?wovn=fr"));
+        assertEquals("fr", sut.getLang("/?wovn=fr"));
+        assertEquals("fr", sut.getLang("/en/?wovn=fr"));
+        assertEquals("fr", sut.getLang("/en/?lang=es&wovn=fr&country=vi"));
+        assertEquals("fr", sut.getLang("en.site.com/es/page/index.html?wovn=fr"));
+    }
 
     public void testRemoveLang__NonMatchingQuery__DoNotModify() {
         QueryUrlLanguagePatternHandler sut = new QueryUrlLanguagePatternHandler();
-		assertEquals("/", sut.removeLang("/", "ja"));
-		assertEquals("/page/index.html", sut.removeLang("/page/index.html", "ja"));
-		assertEquals("/page/?wovn=en", sut.removeLang("/page/?wovn=en", "ja"));
-		assertEquals("ja.site.com/ja/?lang=ja", sut.removeLang("ja.site.com/ja/?lang=ja", "ja"));
+        assertEquals("/", sut.removeLang("/", "ja"));
+        assertEquals("/page/index.html", sut.removeLang("/page/index.html", "ja"));
+        assertEquals("/page/?wovn=en", sut.removeLang("/page/?wovn=en", "ja"));
+        assertEquals("ja.site.com/ja/?lang=ja", sut.removeLang("ja.site.com/ja/?lang=ja", "ja"));
     }
 
     public void testRemoveLang__MatchingQuery__RemoveLangCode() {
         QueryUrlLanguagePatternHandler sut = new QueryUrlLanguagePatternHandler();
-		assertEquals("", sut.removeLang("?wovn=ja", "ja"));
-		assertEquals("/?search=pizza&lang=ja", sut.removeLang("/?search=pizza&wovn=ja&lang=ja", "ja"));
-		assertEquals("site.com/page/index.html?wovn&wovn", sut.removeLang("site.com/page/index.html?wovn&wovn=ja&wovn", "ja"));
-		assertEquals("ja.site.com/ja/", sut.removeLang("ja.site.com/ja/?wovn=ja", "ja"));
+        assertEquals("", sut.removeLang("?wovn=ja", "ja"));
+        assertEquals("/?search=pizza&lang=ja", sut.removeLang("/?search=pizza&wovn=ja&lang=ja", "ja"));
+        assertEquals("site.com/page/index.html?wovn&wovn", sut.removeLang("site.com/page/index.html?wovn&wovn=ja&wovn", "ja"));
+        assertEquals("ja.site.com/ja/", sut.removeLang("ja.site.com/ja/?wovn=ja", "ja"));
     }
 }
--- src/test/java/com/github/wovnio/wovnjava/SubdomainUrlLanguagePatternHandlerTest.java
+++ src/test/java/com/github/wovnio/wovnjava/SubdomainUrlLanguagePatternHandlerTest.java
@@ -17,24 +17,24 @@ public class SubdomainUrlLanguagePatternHandlerTest extends TestCase {
 
     public void testGetLang__MatchingSubdomain__ReturnLangCode() {
         SubdomainUrlLanguagePatternHandler sut = new SubdomainUrlLanguagePatternHandler();
-		assertEquals("en", sut.getLang("en.site.com"));
-		assertEquals("es", sut.getLang("es.site.com/"));
-		assertEquals("fr", sut.getLang("fr.site.com/en/page/index.html?lang=it&wovn=en"));
-	}
+        assertEquals("en", sut.getLang("en.site.com"));
+        assertEquals("es", sut.getLang("es.site.com/"));
+        assertEquals("fr", sut.getLang("fr.site.com/en/page/index.html?lang=it&wovn=en"));
+    }
 
     public void testRemoveLang__NonMatchingSubdomain__DoNotModify() {
         SubdomainUrlLanguagePatternHandler sut = new SubdomainUrlLanguagePatternHandler();
-		assertEquals("/", sut.removeLang("/", "en"));
+        assertEquals("/", sut.removeLang("/", "en"));
         assertEquals("site.com", sut.removeLang("site.com", "en"));
-		assertEquals("ja.site.com", sut.removeLang("ja.site.com", "fr"));
-		assertEquals("ja.fr.site.com", sut.removeLang("ja.fr.site.com", "fr"));
-		assertEquals("site.com/fr/index.html?wovn=fr", sut.removeLang("site.com/fr/index.html?wovn=fr", "fr"));
+        assertEquals("ja.site.com", sut.removeLang("ja.site.com", "fr"));
+        assertEquals("ja.fr.site.com", sut.removeLang("ja.fr.site.com", "fr"));
+        assertEquals("site.com/fr/index.html?wovn=fr", sut.removeLang("site.com/fr/index.html?wovn=fr", "fr"));
     }
 
     public void testRemoveLang__MatchingSubdomain__RemoveLangCode() {
         SubdomainUrlLanguagePatternHandler sut = new SubdomainUrlLanguagePatternHandler();
-		assertEquals("site.com", sut.removeLang("en.site.com", "en"));
-		assertEquals("site.com/", sut.removeLang("es.site.com/", "es"));
-		assertEquals("site.com/fr/index.html?lang=fr&wovn=fr", sut.removeLang("fr.site.com/fr/index.html?lang=fr&wovn=fr", "fr"));
-	}
+        assertEquals("site.com", sut.removeLang("en.site.com", "en"));
+        assertEquals("site.com/", sut.removeLang("es.site.com/", "es"));
+        assertEquals("site.com/fr/index.html?lang=fr&wovn=fr", sut.removeLang("fr.site.com/fr/index.html?lang=fr&wovn=fr", "fr"));
+    }
 }
