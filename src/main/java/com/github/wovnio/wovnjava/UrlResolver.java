def8d016159767c25e108121e356fcfbc6c5091b
Torkel Berli <torkelbe@gmail.com>
2019-07-09 14:21:28 +0900

    Use single line comments

--- src/main/java/com/github/wovnio/wovnjava/UrlResolver.java
+++ src/main/java/com/github/wovnio/wovnjava/UrlResolver.java
@@ -17,20 +17,20 @@ final class UrlResolver {
         String host = null;
         Integer port = null;
         if (useProxy) {
-            /* request.getHeader returns String or null */
+            // request.getHeader returns String or null
             host = request.getHeader("X-Forwarded-Host");
-            /* request.getHeader returns String or null */
+            // request.getHeader returns String or null
             String forwardedPort = request.getHeader("X-Forwarded-Port");
             if (forwardedPort != null && !forwardedPort.isEmpty()) {
                 port = Integer.parseInt(forwardedPort);
             }
         }
         if (host == null) {
-            /* request.getServerName returns String */
+            // request.getServerName returns String
             host = request.getServerName();
         }
         if (port == null) {
-            /* request.getServerPort returns int */
+            // request.getServerPort returns int
             port = request.getServerPort();
         }
 
@@ -44,18 +44,18 @@ final class UrlResolver {
     private static String clientRequestPath(HttpServletRequest request, String originalUrlHeaderSetting) {
         String path = null;
         if (!originalUrlHeaderSetting.isEmpty()) {
-            /* request.getHeader returns String or null */
+            // request.getHeader returns String or null
             path = request.getHeader(originalUrlHeaderSetting);
         }
         if (path == null) {
-            /* request.getAttribute returns Object or null */
+            // request.getAttribute returns Object or null
             Object forwardedPath = request.getAttribute("javax.servlet.forward.request_uri");
             if (forwardedPath != null) {
                 path = forwardedPath.toString();
             }
         }
         if (path == null) {
-            /* request.getRequestURI returns String */
+            // request.getRequestURI returns String
             path = request.getRequestURI();
         }
 
@@ -72,14 +72,14 @@ final class UrlResolver {
             query = request.getHeader(originalQueryStringHeaderSetting);
         }
         if (query == null) {
-            /* request.getAttribute returns Object or null */
+            // request.getAttribute returns Object or null
             Object forwardedQuery = request.getAttribute("javax.servlet.forward.query_string");
             if (forwardedQuery != null) {
                 query = forwardedQuery.toString();
             }
         }
         if (query == null) {
-            /* request.getQueryString returns String or null */
+            // request.getQueryString returns String or null
             query = request.getQueryString();
         }
 
