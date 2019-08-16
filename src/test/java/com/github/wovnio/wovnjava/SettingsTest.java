de8e50f30914fb1e755915e148692d78997394bb
Torkel Berli <torkelbe@gmail.com>
2019-08-16 17:30:59 +0900

    WIP renew SettingsTest

--- src/test/java/com/github/wovnio/wovnjava/SettingsTest.java
+++ src/test/java/com/github/wovnio/wovnjava/SettingsTest.java
@@ -11,6 +11,7 @@ import javax.servlet.FilterConfig;
 
 public class SettingsTest extends TestCase {
 
+    /*
     private static FilterConfig mockEmptyConfig() {
         return TestUtil.makeConfig();
     }
@@ -51,6 +52,189 @@ public class SettingsTest extends TestCase {
         }};
         return TestUtil.makeConfig(parameters);
     }
+    */
+
+    private static FilterConfig makeConfigWithValidBase(HashMap<String, String> extraValues) {
+        HashMap<String, String> settings = new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }};
+        settings.putAll(extrValues);
+        return TestUtil.makeConfig(settings);
+    }
+
+    public void testSettings__MinimalConfiguration__DefaultValuesOK() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }});
+        Settings s = new Settings(config);
+
+        assertEquals("123456", s.projectToken);
+        assertEquals("path", s.urlPattern);
+        assertEquals("en", s.defaultLang);
+
+        ArrayList<String> expectedSupportedLangs = new ArrayList<String>();
+        expectedSupportedLangs.add("en");
+        expectedSupportedLangs.add("ja");
+        assertEquals(expectedSupportedLangs, s.supportedLangs);
+
+        assertEquals(false, s.devMode);
+        assertEquals(false, s.debugMode);
+        assertEquals(false, s.useProxy);
+        assertEquals(false, s.enableFlushBuffer);
+
+        assertEquals("", s.sitePrefixPath);
+        assertEquals("", s.originalUrlHeader);
+        assertEquals("", s.originalQueryStringHeader);
+
+        ArrayList<String> emptyArrayList = new ArrayList<String>();
+        assertEquals(emptyArrayList, s.ignoreClasses);
+
+        assertEquals(Settings.DefaultSnippetUrlProduction, s.snippetUrl);
+        assertEquals(Settings.DefaultApiUrlProduction, s.apiUrl);
+
+        assertEquals(Settings.DefaultTimeout, s.connectTimeout);
+        assertEquals(Settings.DefaultTimeout, s.readTimeout);
+    }
+
+    public void testSettings__MissingProjectToken__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__MissingUrlPattern__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__MissingDefaultLang__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("supportedLangs", "en,ja");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__InvalidDefaultLangCode__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "English");
+            put("supportedLangs", "en,ja");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__MissingSupportedLangs__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__InvalidSupportedLangCode__ThrowError() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,Japanese");
+        }});
+        boolean errorThrown = false;
+        try {
+            Settings s = new Settings(config);
+        } catch (ConfigurationError e) {
+            errorThrown = true;
+        }
+        assertEquals(true, errorThrown);
+    }
+
+    public void testSettings__LegacyUserTokenDeclared__SetProjectTokenAsUserToken() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("userToken", "98765");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }});
+        Settings s = new Settings(config);
+
+        assertEquals("98765", s.projectToken);
+    }
+
+    public void testSettings__BothProjectTokenAndUserTokenDeclared__PreferProjectToken() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("userToken", "98765");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "en,ja");
+        }});
+        Settings s = new Settings(config);
+
+        assertEquals("123456", s.projectToken);
+    }
+
+    public void testSettings__DefaultLangNotInSupportedLangs__AddDefaultLangToList() throws ConfigurationError {
+        FilterConfig config = TestUtil.makeConfig(new HashMap<String, String>() {{
+            put("projectToken", "123456");
+            put("urlPattern", "path");
+            put("defaultLang", "en");
+            put("supportedLangs", "ja");
+        }});
+        Settings s = new Settings(config);
+
+        ArrayList<String> expectedSupportedLangs = new ArrayList<String>();
+        expectedSupportedLangs.add("ja");
+        expectedSupportedLangs.add("en");
+        assertEquals(expectedSupportedLangs, s.supportedLangs);
+    }
+
+    /*
 
     // urlPattern is "path".
     public void testSettingsWithEmptyConfig() throws ConfigurationError {
@@ -156,4 +340,5 @@ public class SettingsTest extends TestCase {
         Settings s = TestUtil.makeSettings(option);
         assertEquals("/global", s.sitePrefixPath);
     }
+    */
 }
