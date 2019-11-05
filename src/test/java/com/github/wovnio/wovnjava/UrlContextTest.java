7126f73f6ec38b2590bc852532c11c66f72a0e3f
Torkel Berli <torkelbe@gmail.com>
2019-11-05 18:38:56 +0900

    Add test case for isSameHost

--- src/test/java/com/github/wovnio/wovnjava/UrlContextTest.java
+++ src/test/java/com/github/wovnio/wovnjava/UrlContextTest.java
@@ -1,6 +1,7 @@
 package com.github.wovnio.wovnjava;
 
 import java.net.URL;
+import java.net.MalformedURLException;
 
 import junit.framework.TestCase;
 
@@ -15,52 +16,58 @@ public class UrlContextTest extends TestCase {
         this.contextPathAndQuery = new UrlContext(new URL("http://site.com/dir/?user=tom"));
     }
 
-    public void testCreateAbsoluteUrl__RelativePath() {
+    public void testResolve__RelativePath() {
         String location = "img/cat.png";
         assertEquals("http://site.com/img/cat.png", this.contextRootPath.resolve(location).toString());
         assertEquals("http://site.com/dir/img/cat.png", this.contextFilePath.resolve(location).toString());
         assertEquals("http://site.com/dir/img/cat.png", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__InvalidDotDotRelativePath() {
+    public void testResolve__InvalidDotDotRelativePath() {
         String location = "../../img/cat.png";
         assertEquals("http://site.com/img/cat.png", this.contextRootPath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png", this.contextFilePath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__AbsolutePath() {
+    public void testResolve__AbsolutePath() {
         String location = "/img/cat.png";
         assertEquals("http://site.com/img/cat.png", this.contextRootPath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png", this.contextFilePath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__AbsoluteUrl() {
+    public void testResolve__AbsoluteUrl() {
         String location = "https://otherdomain.com/img/cat.png";
         assertEquals("https://otherdomain.com/img/cat.png", this.contextRootPath.resolve(location).toString());
         assertEquals("https://otherdomain.com/img/cat.png", this.contextFilePath.resolve(location).toString());
         assertEquals("https://otherdomain.com/img/cat.png", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__AbsoluteUrlWithoutSchema() {
+    public void testResolve__AbsoluteUrlWithoutSchema() {
         String location = "//otherdomain.com/img/cat.png";
         assertEquals("http://otherdomain.com/img/cat.png", this.contextRootPath.resolve(location).toString());
         assertEquals("http://otherdomain.com/img/cat.png", this.contextFilePath.resolve(location).toString());
         assertEquals("http://otherdomain.com/img/cat.png", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__PathAndQuery() {
+    public void testResolve__PathAndQuery() {
         String location = "/img/cat.png?country=japan";
         assertEquals("http://site.com/img/cat.png?country=japan", this.contextRootPath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png?country=japan", this.contextFilePath.resolve(location).toString());
         assertEquals("http://site.com/img/cat.png?country=japan", this.contextPathAndQuery.resolve(location).toString());
     }
 
-    public void testCreateAbsoluteUrl__OnlyQuery() {
+    public void testResolve__OnlyQuery() {
         String location = "?country=japan";
         assertEquals("http://site.com/?country=japan", this.contextRootPath.resolve(location).toString());
         assertEquals("http://site.com/dir/?country=japan", this.contextFilePath.resolve(location).toString());
         assertEquals("http://site.com/dir/?country=japan", this.contextPathAndQuery.resolve(location).toString());
+    }
+
+    public void testIsSameHost() throws MalformedURLException {
+        assertEquals(true, this.contextRootPath.isSameHost(new URL("http://site.com/?country=japan")));
+        assertEquals(false, this.contextRootPath.isSameHost(new URL("http://en.site.com/?country=japan")));
+        assertEquals(false, this.contextRootPath.isSameHost(new URL("http://other.com/?country=japan")));
     }
 }
