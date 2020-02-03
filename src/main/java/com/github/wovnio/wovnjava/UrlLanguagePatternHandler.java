6996348038871964cc03033ae6449e30cc33b6ab
Torkel Berli <torkelbe@gmail.com>
2020-02-03 11:03:58 +0900

    Add comments

--- src/main/java/com/github/wovnio/wovnjava/UrlLanguagePatternHandler.java
+++ src/main/java/com/github/wovnio/wovnjava/UrlLanguagePatternHandler.java
@@ -12,10 +12,29 @@ abstract class UrlLanguagePatternHandler {
      */
     abstract Lang getLang(String url);
 
+    /*
+     * Return the input URL converted into its form in default language
+     *
+     * If the input URL cannot be intercepted (does not match "site prefix path"
+     * for example), return the input URL unchanged.
+     */
     abstract String convertToDefaultLanguage(String url);
 
+    /*
+     * Return the input URL converted into the declared target language
+     *
+     * If the input URL cannot be intercepted (does not match "site prefix path"
+     * for example), return the input URL unchanged.
+     */
     abstract String convertToTargetLanguage(String url, Lang lang);
 
+    /*
+     * Return true if the input URL is identified as being in original language
+     * but another form of the URL is preferred, such that the server should send
+     * a HTTP 302 redirect to the preferred URL for the resource.
+     *
+     * (Currently only relevant for explicitly declared default language in path pattern.)
+     */
     public boolean shouldRedirectExplicitDefaultLangUrl(String url) {
         return false;
     }
