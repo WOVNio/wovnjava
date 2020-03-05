15245addfb6c672af8614db7840d4eb6bf7f4033
Yuji Iwai <iwai@wovn.io>
2020-03-05 16:35:51 +0900

    Change maven docker container

--- docker/java6/maven-clean-package.sh
+++ docker/java6/maven-clean-package.sh
@@ -5,19 +5,18 @@ cd `dirname $0`
 docker network create wovnjava6-maven_default
 
 docker run -d --net wovnjava6-maven_default \
-       -v $(pwd)/conf/squid.conf:/etc/squid/squid.conf \
-       -v $(pwd)/conf/squid_myCA.pem:/etc/squid/squid_myCA.pem \
        --name wovnjava-maven-proxy \
-       iwai/squid-with-openssl
+       wiwai/cci-maven-proxy:3-jdk6
 
 sleep 1
 
 docker run -it --rm --net wovnjava6-maven_default \
-       -v $(pwd):/project \
+       -e "PROXY_HOST=wovnjava-maven-proxy" \
        -v wovnjava6-maven_repo:/root/.m2 \
-       -v $(pwd)/conf/settings.xml:/usr/share/maven/conf/settings.xml \
-       -w /project wovnjava6-maven mvn $@
+       -v $(pwd):/project \
+       -w /project wiwai/cci-maven:3-jdk6 mvn clean package
 
+echo "Cleaning docker containers.."
 docker stop wovnjava-maven-proxy \
     && docker rm wovnjava-maven-proxy \
     && docker volume rm wovnjava6-maven_repo \
--- maven3-jdk6.sh
+++ maven3-jdk6.sh
@@ -2,26 +2,19 @@
 
 cd `dirname $0`
 
-BUILD_IMG=$(docker images -q --filter "reference=wovnjava6-maven")
-if [ -z $BUILD_IMG ]; then
-    docker build -t wovnjava6-maven ./docker/java6
-fi
-
 docker network create wovnjava6-maven_default
 
 docker run -d --net wovnjava6-maven_default \
-       -v $(pwd)/docker/java6/conf/squid.conf:/etc/squid/squid.conf \
-       -v $(pwd)/docker/java6/conf/squid_myCA.pem:/etc/squid/squid_myCA.pem \
        --name wovnjava-maven-proxy \
-       iwai/squid-with-openssl
+       wiwai/cci-maven-proxy:3-jdk6
 
 sleep 1
 
 docker run -it --rm --net wovnjava6-maven_default \
-       -v $(pwd):/project \
+       -e "PROXY_HOST=wovnjava-maven-proxy" \
        -v wovnjava6-maven_repo:/root/.m2 \
-       -v $(pwd)/docker/java6/conf/settings.xml:/usr/share/maven/conf/settings.xml \
-       -w /project wovnjava6-maven mvn $@
+       -v $(pwd):/project \
+       -w /project wiwai/cci-maven:3-jdk6 mvn $@
 
 echo "Cleaning docker containers.."
 docker stop wovnjava-maven-proxy \
