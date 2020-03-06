d3bb0c909ac6edcfe0e63dd28b033f3be5644f82
Yuji Iwai <iwai@wovn.io>
2020-03-06 14:00:16 +0900

    Change hello wovnjava build scripts

--- docker/java6/maven-clean-package.sh
+++ docker/java6/maven-clean-package.sh
@@ -2,22 +2,26 @@
 
 cd `dirname $0`
 
+# when if local compiled library exists, that library use with compile.
+if [ -e ../../target/wovnjava-1.0.1-jdk6-jar-with-dependencies.jar ]; then
+    mkdir -p hello/lib
+    cp ../../target/wovnjava-1.0.1-jdk6-jar-with-dependencies.jar hello/lib/
+fi
+
 docker network create wovnjava6-maven_default
 
-docker run -d --net wovnjava6-maven_default \
-       --name wovnjava-maven-proxy \
-       wiwai/cci-maven-proxy:3-jdk6
+
+proxy_running=$(docker ps --filter "name=wovnjava-maven-proxy" -q)
+if [ -z "$proxy_running" ]; then
+    docker run -d --net wovnjava6-maven_default \
+           --name wovnjava-maven-proxy \
+           wiwai/cci-maven-proxy:3-jdk6
+fi
 
 sleep 1
 
 docker run -it --rm --net wovnjava6-maven_default \
        -e "PROXY_HOST=wovnjava-maven-proxy" \
        -v wovnjava6-maven_repo:/root/.m2 \
-       -v $(pwd):/project \
-       -w /project wiwai/cci-maven:3-jdk6 mvn clean package
-
-echo "Cleaning docker containers.."
-docker stop wovnjava-maven-proxy \
-    && docker rm wovnjava-maven-proxy \
-    && docker volume rm wovnjava6-maven_repo \
-    && docker network rm wovnjava6-maven_default
+       -v $(pwd)/hello:/project \
+       -w /project wiwai/cci-maven:3-jdk6 mvn -e package
