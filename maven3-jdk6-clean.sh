235edb56feaccd6184217576cedc6f680a928c7f
Yuji Iwai <iwai@wovn.io>
2020-03-06 13:59:36 +0900

    Improve container cleaner

--- maven3-jdk6-clean.sh
+++ maven3-jdk6-clean.sh
@@ -2,6 +2,6 @@
 
 echo "Cleaning docker containers.."
 docker stop wovnjava-maven-proxy \
-    && docker rm wovnjava-maven-proxy \
-    && docker volume rm wovnjava6-maven_repo \
-    && docker network rm wovnjava6-maven_default
+    && docker rm wovnjava-maven-proxy
+docker volume rm wovnjava6-maven_repo
+docker network rm wovnjava6-maven_default
