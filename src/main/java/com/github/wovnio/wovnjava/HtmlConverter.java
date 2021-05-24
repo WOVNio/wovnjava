f3da091343d629d8df691d1374c71f871a3413ee
Zeyu Wu <zeyu.wu@wovn.io>
2021-05-24 10:49:17 +0900

    added hreflang to non-source language pages.

--- src/main/java/com/github/wovnio/wovnjava/HtmlConverter.java
+++ src/main/java/com/github/wovnio/wovnjava/HtmlConverter.java
@@ -38,6 +38,7 @@ class HtmlConverter {
         removeWovnIgnore();
         removeClassIgnore();
         removeForm();
+        appendHrefLang();
         insertHtmlLangAttribute();
         return doc.html();
     }
