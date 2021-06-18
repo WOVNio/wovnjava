890e5b2cda6e2fb438f92d74d86ec31c8a0b022d
Zeyu Wu <zeyu.wu@wovn.io>
2021-06-18 16:31:30 +0900

    updated tests to actually test for encoding detection.

--- src/test/java/com/github/wovnio/wovnjava/Utf8Test.java
+++ src/test/java/com/github/wovnio/wovnjava/Utf8Test.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.assertNotEquals;
 
 import java.io.UnsupportedEncodingException;
 
+import org.junit.Ignore;
+
 import junit.framework.TestCase;
 
 public class Utf8Test extends TestCase {
     public void testDefaultEncodingUTF8() {
         Utf8 unicodeConverter = new Utf8("");
-        String data = new String("Unicøde Sentence　東京");
+        String data = new String("Unicøde Sentence　東京 الخط العربي في يونيكود");
         byte[] rawData = null;
         try {
             rawData = data.getBytes("utf-8");
@@ -17,6 +19,31 @@ public class Utf8Test extends TestCase {
             System.exit(1);
         } 
         assertEquals(data, unicodeConverter.toStringUtf8(rawData));
+    }
+
+    public void testDefaultEncodingShiftJIS() {
+        Utf8 unicodeConverter = new Utf8("");
+        String data = new String("新字体 関	鉄 気 １２３");
+        byte[] rawData = null;
+        try {
+            rawData = data.getBytes("Shift_JIS");
+        } catch (UnsupportedEncodingException e) {
+            System.exit(1);
+        } 
+        assertEquals(data, unicodeConverter.toStringUtf8(rawData));
+    }
+
+
+    public void testDefaultEncodingEUCJP__expected_to_fail() {
+        Utf8 unicodeConverter = new Utf8("");
+        String data = new String("新字体 関	鉄 気１２３");
+        byte[] rawData = null;
+        try {
+            rawData = data.getBytes("EUC-JP");
+        } catch (UnsupportedEncodingException e) {
+            System.exit(1);
+        } 
+        assertNotEquals(data, unicodeConverter.toStringUtf8(rawData));
     }
 
     public void testWrongEncodingProvidedShouldNotDecode() {
