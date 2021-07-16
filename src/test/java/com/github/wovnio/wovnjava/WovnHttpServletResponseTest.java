30e3438b9c33c3dcbdc36eee140dffa379cf7397
Zeyu Wu <11744696+zeyuwu@users.noreply.github.com>
2021-07-16 10:39:39 +0900

    Fixed incorrect string quality check (#182)
    * Fixed incorrect string quality check
    
    * Bump patch version.
    
    * Added unit test for WovnResponse.
    
    * Improves test cases

---
+++ src/test/java/com/github/wovnio/wovnjava/WovnHttpServletResponseTest.java
@@ -0,0 +1,39 @@
+package com.github.wovnio.wovnjava;
+
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+
+public class WovnHttpServletResponseTest extends TestCase {
+    private WovnHttpServletResponse response;
+    private Headers headersMock;
+
+    private void setup() {
+        this.headersMock = EasyMock.createMock(Headers.class);
+        Utf8 unicodeConverter = new Utf8("Utf-8");
+        this.response = new WovnHttpServletResponse(EasyMock.createMock(HttpServletResponse.class), headersMock, unicodeConverter);
+    }
+
+    public void testSetHeader() {
+        this.setup();
+        EasyMock.expect(this.headersMock.locationWithLangCode("/nice.html")).andReturn("ja/nice.html").once();
+        EasyMock.replay(this.headersMock);
+        this.response.setHeader("location", "/nice.html");
+    }
+
+    public void testSetHeader__should_ignore_case() {
+        this.setup();
+        EasyMock.expect(this.headersMock.locationWithLangCode("/nice.html")).andReturn("ja/nice.html").once();
+        EasyMock.replay(this.headersMock);
+        this.response.setHeader("LOCAtion", "/nice.html");
+    }
+
+    public void testSetHeader__should_not_match_non_location() {
+        this.setup();
+        EasyMock.replay(this.headersMock);
+        this.response.setHeader("Lotion", "/nice.html");
+        this.response.setHeader("X-Location", "/nice.html");
+        this.response.setHeader("Locations", "/nice.html");
+    }
+}
