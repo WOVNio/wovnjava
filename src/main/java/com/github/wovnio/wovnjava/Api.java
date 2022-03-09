4678cc555ed1145a46de5583197de5b96ce63c55
Zeyu Wu <zeyu.wu@wovn.io>
2022-03-09 12:33:48 +0900

    added StringUtilsTest

--- src/main/java/com/github/wovnio/wovnjava/Api.java
+++ src/main/java/com/github/wovnio/wovnjava/Api.java
@@ -233,24 +233,9 @@ class Api {
         MessageDigest md = MessageDigest.getInstance("MD5");
         md.update(item);
         byte[] digest = md.digest();
-        return this.encodeHexString(digest).toUpperCase();
+        return StringUtils.encodeHexString(digest).toUpperCase();
     }
-
-    private String encodeHexString(byte[] byteArray) {
-      StringBuffer hexStringBuffer = new StringBuffer();
-      for (int i = 0; i < byteArray.length; i++) {
-          hexStringBuffer.append(byteToHex(byteArray[i]));
-      }
-      return hexStringBuffer.toString();
-    }
-
-    private String byteToHex(byte num) {
-      char[] hexDigits = new char[2];
-      hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
-      hexDigits[1] = Character.forDigit((num & 0xF), 16);
-      return new String(hexDigits);
-  }
-
+    
     private void appendValue(StringBuilder sb, String value) throws UnsupportedOperationException {
         sb.append(FormUrlEncoding.encodeValue(value));
     }
---
+++ src/main/java/com/github/wovnio/wovnjava/StringUtils.java
@@ -0,0 +1,18 @@
+package com.github.wovnio.wovnjava;
+
+public class StringUtils {
+  public static String encodeHexString(byte[] byteArray) {
+    StringBuffer hexStringBuffer = new StringBuffer();
+    for (int i = 0; i < byteArray.length; i++) {
+      hexStringBuffer.append(byteToHex(byteArray[i]));
+    }
+    return hexStringBuffer.toString();
+  }
+
+  private static String byteToHex(byte num) {
+    char[] hexDigits = new char[2];
+    hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+    hexDigits[1] = Character.forDigit((num & 0xF), 16);
+    return new String(hexDigits);
+  }
+}
