b8ad4669bdff2690b776a6f2fa32b505f30e2544
bstewart <brett@wovn.io>
2022-07-06 15:38:37 +0900

    fix tests

--- src/test/java/com/github/wovnio/wovnjava/TestUtil.java
+++ src/test/java/com/github/wovnio/wovnjava/TestUtil.java
@@ -63,7 +63,6 @@ public class TestUtil {
 
     public static HttpServletResponse mockResponse(String contentType, String encoding, boolean isPreviouslyProcessed, int statusCode) throws IOException {
         HttpServletResponse mock = EasyMock.createMock(HttpServletResponse.class);
-        EasyMock.expectLastCall();
         mock.setCharacterEncoding("utf-8");
         EasyMock.expectLastCall();
         EasyMock.expect(mock.getWriter()).andReturn(new PrintWriter(new StringWriter()));
